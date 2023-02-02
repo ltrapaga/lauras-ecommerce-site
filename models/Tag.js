b30db2f638082f0197f7e18154d6cc692b8a4fe7
Laura Trapaga <trapaga9@gmail.com>
2023-02-01 21:06:02 -0500

    Edit Tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -4,10 +4,17 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
-Tag.init(
-  {
-    // define columns
+Tag.init({
+  id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
   },
+  tag_name: {
+      type: DataTypes.STRING,
+  },
+}, 
   {
     sequelize,
     timestamps: false,
