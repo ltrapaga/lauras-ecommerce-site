08a442940126225e5ab2f30012a07855e386f05e
Laura Trapaga <trapaga9@gmail.com>
2023-02-01 21:36:34 -0500

    Add GET requests in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,22 +1,49 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get("/", async (req, res) => {
+  try {
+    // find all products
+    const productData = await Product.findAll({
+      // be sure to include its associated Category and Tag data
+      include: [
+        { model: Category, as: "category" },
+        { model: Tag, through: ProductTag, as: "tagIds" },
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get("/:id", async (req, res) => {
+  try {
+    // find a single product by its `id`
+    const productDataById = await Product.findByPk(req.params.id, {
+      // be sure to include its associated Category and Tag data
+      include: [
+        { model: Category, as: "category" },
+        { model: Tag, through: ProductTag, as: "tagIds" },
+      ],
+    });
+
+    if (!productDataById) {
+      res.status(404).json({ message: "No product found with that id!" });
+      return;
+    }
+    res.status(200).json(productDataById);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +75,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,7 +116,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
 });
 
